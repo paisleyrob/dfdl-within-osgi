a5f8a1e8696e0a239cf092aea038f456de986f8c
Rob Paisley <rpaisley@harris.com>
2017-07-24 15:20:59 -0400

    Use Thread Context Class Loader for schema lookup

--- osgi.xml.impl/src/main/java/com/rpaisley/example/osgi/xml/impl/XMLServiceImpl.java
+++ osgi.xml.impl/src/main/java/com/rpaisley/example/osgi/xml/impl/XMLServiceImpl.java
@@ -7,9 +7,16 @@ public class XMLServiceImpl {
 	public XMLServiceImpl() {
 		System.out.println(this.getClass().getName() + " starting");
 		try {
-			SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-			System.out.println("Lookup of schema: " + XMLConstants.W3C_XML_SCHEMA_NS_URI
-					+ " yielded factory: " + factory.getClass().getName());
+			ClassLoader original = Thread.currentThread().getContextClassLoader();
+			try {
+				Thread.currentThread().setContextClassLoader(XMLServiceImpl.class.getClassLoader());
+				SchemaFactory factory = SchemaFactory
+						.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+				System.out.println("Lookup of schema: " + XMLConstants.W3C_XML_SCHEMA_NS_URI
+						+ " yielded factory: " + factory.getClass().getName());
+			} finally {
+				Thread.currentThread().setContextClassLoader(original);
+			}
 		} catch (Throwable t) {
 			System.out.println("Failed to lookup schema: " + t);
 
