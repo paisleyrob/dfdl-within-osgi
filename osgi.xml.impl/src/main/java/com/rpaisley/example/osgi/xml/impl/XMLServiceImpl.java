04f155b94a6c73d23d593691314826fa85d19252
Rob Paisley <rpaisley@harris.com>
2017-08-08 15:22:01 -0400

    Have XML use ThreadService and LogService

--- osgi.xml.impl/src/main/java/com/rpaisley/example/osgi/xml/impl/XMLServiceImpl.java
+++ osgi.xml.impl/src/main/java/com/rpaisley/example/osgi/xml/impl/XMLServiceImpl.java
@@ -3,23 +3,41 @@ package com.rpaisley.example.osgi.xml.impl;
 import javax.xml.XMLConstants;
 import javax.xml.validation.SchemaFactory;
 
-public class XMLServiceImpl {
-	public XMLServiceImpl() {
-		System.out.println(this.getClass().getName() + " starting");
+import org.osgi.service.log.LogService;
+
+import com.rpaisley.example.osgi.thread.ThreadService;
+
+public class XMLServiceImpl extends ThreadService {
+	private volatile LogService logService;
+
+	@Override
+	protected void setup() {
+		logService.log(LogService.LOG_DEBUG, this.getClass().getName() + " starting");
 		try {
 			ClassLoader original = Thread.currentThread().getContextClassLoader();
 			try {
 				Thread.currentThread().setContextClassLoader(XMLServiceImpl.class.getClassLoader());
 				SchemaFactory factory = SchemaFactory
 						.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-				System.out.println("Lookup of schema: " + XMLConstants.W3C_XML_SCHEMA_NS_URI
-						+ " yielded factory: " + factory.getClass().getName());
+				logService.log(LogService.LOG_DEBUG,
+						"Lookup of schema: " + XMLConstants.W3C_XML_SCHEMA_NS_URI
+								+ " yielded factory: " + factory.getClass().getName());
 			} finally {
 				Thread.currentThread().setContextClassLoader(original);
 			}
 		} catch (Throwable t) {
-			System.out.println("Failed to lookup schema: " + t);
+			logService.log(LogService.LOG_DEBUG, "Log me stuff");
+		}
+	}
 
+	@Override
+	protected void process() {
+		try {
+			logService.log(LogService.LOG_DEBUG, "Nothing left to do, waiting()");
+			synchronized (this) {
+				wait();
+			}
+		} catch (InterruptedException e) {
 		}
 	}
 }
